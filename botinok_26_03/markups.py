4b69b3cf6c72422969a8b2ee9681134fdefa9297
Vlad Ershov <125573279+redn1ghtz@users.noreply.github.com>
2023-03-26 18:53:58 +0500

    Add files via upload

---
+++ botinok_26_03/markups.py
@@ -0,0 +1,16 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from main import get_CH_ID1, get_CH_ID2
+
+btnUrlChannel1 = InlineKeyboardButton(text="ПОДПИСАТЬСЯ", url="https://t.me/" + get_CH_ID1())
+btnDoneSub1 = InlineKeyboardButton(text="ПОДПИСАЛСЯ ✅", callback_data="subchanneldone1")
+
+btnUrlChannel2 = InlineKeyboardButton(text="ПОДПИСАТЬСЯ", url="https://t.me/" + get_CH_ID2())
+btnDoneSub2 = InlineKeyboardButton(text="ПОДПИСАЛСЯ ✅", callback_data="subchanneldone2")
+
+checkSubMenu1 = InlineKeyboardMarkup(row_width=1)
+checkSubMenu1.insert(btnUrlChannel1)
+checkSubMenu1.insert(btnDoneSub1)
+
+checkSubMenu2 = InlineKeyboardMarkup(row_width=1)
+checkSubMenu2.insert(btnUrlChannel2)
+checkSubMenu2.insert(btnDoneSub2)
