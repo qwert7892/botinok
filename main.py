59f97ff2c23d53b10a46eaa39d1b627b183cafd3
ilkzkoff <40799490+qwert7892@users.noreply.github.com>
2023-03-28 22:42:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,37 +3,14 @@ import markups as nav
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters.state import StatesGroup, State
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import state
 
 from chatgpt import *
 
 from db import *
 
-
-def get_tgAPI():
-    sf = open('settings.txt', 'r')
-    setfile = sf.readlines()
-    return setfile[0].split()[1]
-
-
-def get_CH_ID1():
-    sf = open('settings.txt', 'r')
-    setfile = sf.readlines()
-    return setfile[2].split()[1]
-
-
-def get_CH_ID2():
-    sf = open('settings.txt', 'r')
-    setfile = sf.readlines()
-    return setfile[3].split()[1]
-
+from settings import *
 
 API_TOKEN = get_tgAPI()
-CH_ID1 = '@' + get_CH_ID1()
-CH_ID2 = '@' + get_CH_ID2()
-MSG_NOT_FOLLOW = f'Хорошо, сейчас я подумаю над твоим вопросом. А ты пока подпишись на канал:\nhttps://t.me/{get_CH_ID1()}'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -46,8 +23,26 @@ file = open('db.txt', '+a')
 file.close()
 
 
-class Profile(StatesGroup):
-    answer = State()
+@dp.message_handler(commands='admin')
+async def admin(message: types.Message):
+    password = message.text.split()[1]
+    com = message.text.split()[2]
+    content = message.text.split()[3]
+    if password == '1':
+        if com == '/add_channel':
+            add_channel(content)
+            await bot.send_message(chat_id=message.chat.id, text='Канал добавлен успешно')
+        elif com == '/delete_channel':
+            delete_channel(content)
+            await bot.send_message(chat_id=message.chat.id, text='Канал удален успешно')
+        elif com == '/change_gpt_key':
+            change_gpt_key(content)
+            await bot.send_message(chat_id=message.chat.id, text='Ключ обновлен успешно')
+            openai.api_key = get_ChatGPT_API()
+        else:
+            await bot.send_message(chat_id=message.chat.id, text='Несуществующая команда')
+    else:
+        await bot.send_message(chat_id=message.chat.id, text='Неверный пароль')
 
 
 @dp.message_handler(commands='start')
@@ -57,41 +52,43 @@ async def send_welcome(message: types.Message):
 Примеры вопросов/запросов:\n~ Сколько будет 7 * 8?\n~ Когда началась Вторая Мировая?\n~ Напиши код калькулятора на Python\n~ Напиши сочинение как я провел лето\n\n\
 🔥 Чтобы начать общение, напиши что-нибудь CHATGPT в строку ниже 👇🏻'
     add_user(str(message.chat.id))
-    await message.reply(a)
+    await bot.send_message(chat_id=message.chat.id, text=a)
 
 
 @dp.message_handler(lambda message: message.text == 'ПОДПИСАЛСЯ ✅')
-async def got_sub(message: types.Message, state: FSMContext):
-    if check_sub_channel(await bot.get_chat_member(chat_id=CH_ID1, user_id=message.from_user.id)):
-        change_status(str(message.chat.id))
-        await message.reply(text='Красавчик блеа')
-        # data = await state.get_data()
-        # data = data['profile_answer']
-        # a = chat_ai(data)
-        # await message.answer(a)
-
-    else:
-        await message.reply('нефор бле')
-
-
-@dp.message_handler(lambda message: message.text == 'ПОДПИСАЛСЯ ✅')
-async def got_sub2(message: types.Message):
-    # await message.reply(MSG_NOT_FOLLOW, reply_markup=nav.bot_sub_checker)
-    if check_sub_channel(await bot.get_chat_member(chat_id=CH_ID2, user_id=message.from_user.id)):
-        change_status_2(str(message.chat.id))
-        await message.reply(text='Красавчик блеа')
-    else:
-        await message.reply('нефор бле')
+async def got_sub(message: types.Message):
+    status = False
+    a = []
+    for i in get_channels():
+        if check_sub_channel(await bot.get_chat_member(chat_id=f'@{i}', user_id=message.from_user.id)):
+            change_status(str(message.chat.id))
+            a.append(True)
+        else:
+            a.append(False)
+            await bot.send_message(chat_id=message.chat.id, text='Ты подписан не на все каналы')
+            break
+    status = True
+    for i in a:
+        if i is False:
+            status = False
+    if status is True:
+        await bot.send_message(chat_id=message.chat.id,
+                               text='Спасибо, теперь можете пользоваться ChatGPT неограниченно')
 
 
 @dp.message_handler()
-async def start_chat(message: types.Message, state: FSMContext):
+async def start_chat(message: types.Message):
     if add_user(str(message.chat.id)) is False and int(get_count(str(message.chat.id))) < 3:
         count_user(str(message.chat.id))
 
         if str(get_count(str(message.chat.id))) == '3':
             a = chat_ai(message.text)
             await bot.send_message(chat_id=message.chat.id, text=a)
+            mes = ''
+            channels = get_channels()
+            for i in channels:
+                mes += f'\n@{i}'
+            MSG_NOT_FOLLOW = f'Чтобы продолжить использовать ChatGPT подпишись на эти каналы:' + mes
             await bot.send_message(chat_id=message.chat.id, text=MSG_NOT_FOLLOW, reply_markup=nav.bot_sub_checker)
 
         else:
@@ -100,7 +97,7 @@ async def start_chat(message: types.Message, state: FSMContext):
 
     elif get_status(str(message.chat.id)) is True and int(get_count(str(message.chat.id))) >= 3:
         a = chat_ai(message.text)
-        await bot.send_message(chat_id=message.chat.id, text=a)
+        await bot.send_message(chat_id=message.chat.id, text=a, reply_markup=types.ReplyKeyboardRemove())
 
 
 def check_sub_channel(chat_member):
@@ -108,26 +105,6 @@ def check_sub_channel(chat_member):
         return True
     else:
         return False
-
-
-@dp.callback_query_handler(text='subchanneldone1')
-async def subchanneldone1(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message.message_id)
-    if check_sub_channel(await bot.get_chat_member(chat_id=CH_ID1, user_id=message.from_user.id)):
-        await bot.send_message(message.from_user.id, 'success!')
-        change_status(str(message.from_user.id))
-    else:
-        await bot.send_message(message.from_user.id, MSG_NOT_FOLLOW, reply_markup=nav.checkSubMenu1)
-
-
-@dp.callback_query_handler(text='subchanneldone2')
-async def subchanneldone1(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message.message_id)
-    if check_sub_channel(await bot.get_chat_member(chat_id=CH_ID2, user_id=message.from_user.id)):
-        await bot.send_message(message.from_user.id, 'success!')
-        change_status_2(str(message.from_user.id))
-    else:
-        await bot.send_message(message.from_user.id, MSG_NOT_FOLLOW, reply_markup=nav.checkSubMenu2)
 
 
 if __name__ == '__main__':
