1ffb33d9a115ce002a4fd2f5d3103ce82eb2e1ea
ilkzkoff <40799490+qwert7892@users.noreply.github.com>
2023-04-05 19:39:45 +0300

    Delete botinok3.0 directory

--- botinok3.0/markups.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-btnSubed = KeyboardButton(text='ПОДПИСАЛСЯ ✅')
-bot_sub_checker = ReplyKeyboardMarkup(resize_keyboard=True)
-bot_sub_checker.add(btnSubed)
